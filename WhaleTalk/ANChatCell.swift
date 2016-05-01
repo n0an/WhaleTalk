d9badea685f62f4ddb1c618229d7c8566a7dd7bb
n0an <nantog@gmail.com>
2016-05-01 22:01:29 +0300

    Bubbles flipping and coloring

--- WhaleTalk/ANChatCell.swift
+++ WhaleTalk/ANChatCell.swift
@@ -42,12 +42,14 @@ class ANChatCell: UITableViewCell {
         messageLabel.numberOfLines = 0
         
         
-        // !!!IMPORTANT!!!
-        // CHANGE COLOR OF IMAGE FROM CODE
-        let image = UIImage(named: "MessageBubble")?.imageWithRenderingMode(.AlwaysTemplate)
-        bubbleImageView.tintColor = UIColor.blueColor()
         
-        bubbleImageView.image = image
+        
+//        // !!!IMPORTANT!!!
+//        // CHANGE COLOR OF IMAGE FROM CODE
+//        let image = UIImage(named: "MessageBubble")?.imageWithRenderingMode(.AlwaysTemplate)
+//        bubbleImageView.tintColor = UIColor.blueColor()()
+//        
+//        bubbleImageView.image = image
         
 
         
@@ -64,21 +66,76 @@ class ANChatCell: UITableViewCell {
         if incoming {
             incomingConstraint.active = true
             outgoingConstraint.active = false
+            
+            bubbleImageView.image = bubble.incoming
+            
         } else {
             incomingConstraint.active = false
             outgoingConstraint.active = true
+            
+            bubbleImageView.image = bubble.outgoing
+
         }
         
     }
-    
-    
-    
-    
-    
-    
+   
     
 
 }
+
+let bubble = makeBubble()
+
+
+func makeBubble() -> (incoming: UIImage, outgoing: UIImage) {
+    
+    let image = UIImage(named: "MessageBubble")!
+    
+    let outgoing = coloredImage(image, red: 0/255, green: 122/255, blue: 255/255, alpha: 1)
+    
+    // !!!IMPORTANT!!!
+    // FLIP IMAGE
+    let flippedImage = UIImage(CGImage: image.CGImage!, scale: image.scale, orientation: .UpMirrored)
+    
+    let incoming = coloredImage(flippedImage, red: 229/255, green: 229/255, blue: 229/255, alpha: 1)
+    
+    return (incoming, outgoing)
+    
+}
+
+
+// !!!IMPORTANT!!!
+// CHANGE COLOR OF IMAGE USING CONTEXT DRAW
+
+func coloredImage(image: UIImage, red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIImage! {
+    
+    let rect = CGRect(origin: CGPointZero, size: image.size)
+    
+    UIGraphicsBeginImageContextWithOptions(image.size, false, image.scale)
+    
+    let context = UIGraphicsGetCurrentContext()
+    
+    image.drawInRect(rect)
+    
+    
+    CGContextSetRGBFillColor(context, red, green, blue, alpha)
+    CGContextSetBlendMode(context, CGBlendMode.SourceAtop)
+    
+    CGContextFillRect(context, rect)
+    
+    let result = UIGraphicsGetImageFromCurrentImageContext()
+    
+    UIGraphicsEndImageContext()
+    
+    return result
+    
+    
+}
+
+
+
+
+
+
 
 
 
