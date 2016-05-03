1f30cad35363e5268f528ae6cb852fb5e7014705
n0an <nantog@gmail.com>
2016-05-03 22:03:24 +0300

    Listen for changes. CNContactStoreDidChangeNotification

--- WhaleTalk/ContactImporter.swift
+++ WhaleTalk/ContactImporter.swift
@@ -10,14 +10,36 @@ import Foundation
 import CoreData
 import Contacts
 
-class ContactImporter {
+class ContactImporter: NSObject {
     
     private var context: NSManagedObjectContext
+    
+    private var lastCNNotificationTime: NSDate?
     
     init(context: NSManagedObjectContext) {
     
         self.context = context
         
+    }
+    
+    
+    func listenForChanges() {
+        CNContactStore.authorizationStatusForEntityType(.Contacts)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "addressBookDidChange:", name: CNContactStoreDidChangeNotification, object: nil)
+        
+        
+        
+    }
+    
+    func addressBookDidChange(notification: NSNotification) {
+        print(notification)
+        
+        let now = NSDate()
+        
+        guard lastCNNotificationTime == nil || now.timeIntervalSinceDate(lastCNNotificationTime!) > 1 else {return}
+        
+        lastCNNotificationTime = now
     }
     
     func formatPhoneNumber(number: CNPhoneNumber) -> String {
@@ -30,43 +52,49 @@ class ContactImporter {
         let store = CNContactStore()
         store.requestAccessForEntityType(.Contacts, completionHandler: { (granted, error) in
             
-            if granted {
-                do {
-                    let req = CNContactFetchRequest(keysToFetch:
-                        [CNContactGivenNameKey,
-                            CNContactFamilyNameKey,
-                            CNContactPhoneNumbersKey])
-                    
-                    try store.enumerateContactsWithFetchRequest(req, usingBlock: { (cnContact, stop) in
-                        print(cnContact)
+            self.context.performBlock{
+                
+                if granted {
+                    do {
+                        let req = CNContactFetchRequest(keysToFetch:
+                            [CNContactGivenNameKey,
+                                CNContactFamilyNameKey,
+                                CNContactPhoneNumbersKey])
                         
-                        guard let contact = NSEntityDescription.insertNewObjectForEntityForName("Contact", inManagedObjectContext: self.context) as? Contact else {return}
-                        
-                        contact.firstName = cnContact.givenName
-                        contact.lastName = cnContact.familyName
-                        contact.contactId = cnContact.identifier
-                        
-                        let contactNumbers = NSMutableSet()
-                        for cnVal in cnContact.phoneNumbers {
-                            guard let cnPhoneNumber = cnVal.value as? CNPhoneNumber else {continue}
+                        try store.enumerateContactsWithFetchRequest(req, usingBlock: { (cnContact, stop) in
+                            print(cnContact)
                             
-                            guard let phoneNumber = NSEntityDescription.insertNewObjectForEntityForName("PhoneNumber", inManagedObjectContext: self.context) as? PhoneNumber else {continue}
+                            guard let contact = NSEntityDescription.insertNewObjectForEntityForName("Contact", inManagedObjectContext: self.context) as? Contact else {return}
                             
-                            phoneNumber.value = self.formatPhoneNumber(cnPhoneNumber)
-                            contactNumbers.addObject(phoneNumber)
-                        }
+                            contact.firstName = cnContact.givenName
+                            contact.lastName = cnContact.familyName
+                            contact.contactId = cnContact.identifier
+                            
+
+                            for cnVal in cnContact.phoneNumbers {
+                                guard let cnPhoneNumber = cnVal.value as? CNPhoneNumber else {continue}
+                                
+                                guard let phoneNumber = NSEntityDescription.insertNewObjectForEntityForName("PhoneNumber", inManagedObjectContext: self.context) as? PhoneNumber else {continue}
+                                
+                                phoneNumber.value = self.formatPhoneNumber(cnPhoneNumber)
+
+                                phoneNumber.contact = contact
+                            }
+                            
+
+                        })
                         
-                        contact.phoneNumbers = contactNumbers
-                    })
-                    
-                    
-                    
-                } catch let error as NSError {
-                    print(error)
-                } catch {
-                    print("Error with do-catch")
+                        try self.context.save()
+                        
+                    } catch let error as NSError {
+                        print(error)
+                    } catch {
+                        print("Error with do-catch")
+                    }
                 }
+            
             }
+            
             
         })
         
